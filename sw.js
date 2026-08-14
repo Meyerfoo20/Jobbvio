d4229e2aa8b7fd06cdc8e6972c178ffad6bdedf5
Meyerfoo20 <158509755+Meyerfoo20@users.noreply.github.com>
2026-08-14 16:27:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'jobbvio-v4';
+const CACHE_NAME = 'jobbvio-v5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './logo.png'
+  './images/logo.png'
 ];
 
 // Installera Service Worker och cacha resurser
